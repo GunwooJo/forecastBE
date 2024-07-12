33b7fc16f444e50e6fae36b0cd56730731b3a50d
GunwooJo <kanggi1997@naver.com>
2024-07-12 19:36:15 +0900

    initial commit

---
+++ src/main/java/site/gunwoo/forecastBE/ForecastBeApplication.java
@@ -0,0 +1,13 @@
+package site.gunwoo.forecastBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForecastBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForecastBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/gunwoo/forecastBE/ForecastBeApplicationTests.java
@@ -0,0 +1,13 @@
+package site.gunwoo.forecastBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForecastBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

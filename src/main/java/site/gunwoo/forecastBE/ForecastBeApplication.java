fd6a3cdce5ca5968e14e8ef2cbe30272e9c9581a
GunwooJo <kanggi1997@naver.com>
2024-07-24 02:52:06 +0900

    Feat: 단기예보 엔티티 정의

--- src/main/java/site/gunwoo/forecastBE/ForecastBeApplication.java
+++ src/main/java/site/gunwoo/forecastBE/ForecastBeApplication.java
@@ -2,8 +2,10 @@ package site.gunwoo.forecastBE;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ForecastBeApplication {
 
 	public static void main(String[] args) {

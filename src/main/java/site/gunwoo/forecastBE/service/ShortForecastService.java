6017aa0d3e17676e32ee2dbb47176c690bac35fc
GunwooJo <kanggi1997@naver.com>
2024-08-01 00:21:40 +0900

    Refactor: 단기예보 조회에 대한 예외처리

--- src/main/java/site/gunwoo/forecastBE/service/ShortForecastService.java
+++ src/main/java/site/gunwoo/forecastBE/service/ShortForecastService.java
@@ -1,6 +1,7 @@
 package site.gunwoo.forecastBE.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,8 +9,7 @@ import org.springframework.web.reactive.function.client.WebClientResponseExcepti
 import site.gunwoo.forecastBE.config.ForecastConstants;
 import site.gunwoo.forecastBE.dto.ShortForeCastResponseDTO;
 import site.gunwoo.forecastBE.entity.ShortForecast;
-import site.gunwoo.forecastBE.exception.CustomClientException;
-import site.gunwoo.forecastBE.exception.CustomServerException;
+import site.gunwoo.forecastBE.exception.ShortForecastException;
 import site.gunwoo.forecastBE.repository.ShortForecastRepository;
 
 import java.time.LocalDate;
@@ -20,14 +20,13 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class ShortForecastService {
 
     private final WebClient.Builder webClientBuilder;
     private final ShortForecastRepository shortForecastRepository;
 
     //강수확률을 알기 위한 단기예보 조회 후 저장.
-    //조회하는 코드와 저장하는 코드를 분리하자.
-    //아니면 category로 분류하지 말고 그냥 다 저장할까?
     public void saveShortForecast(String baseDate, String baseTime, int numOfRows, int pageNo, int nx, int ny) {
 
         WebClient webClient = webClientBuilder
@@ -53,35 +52,46 @@ public class ShortForecastService {
                     .bodyToMono(ShortForeCastResponseDTO.class)
                     .block();
 
-            if(response != null) {
-                DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
-                DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HHmm");
-
-                List<ShortForeCastResponseDTO.Response.Body.Items.ForecastItem> itemList = response.getResponse().getBody().getItems().getItem();
-
-                List<ShortForecast> forecastItems = itemList.stream()
-                        .filter(forecastItem -> "POP".equals(forecastItem.getCategory()))
-                        .map(forecastItem -> {
-                            return ShortForecast.builder()
-                                    .baseDate(LocalDate.parse(forecastItem.getBaseDate(), dateFormatter))
-                                    .baseTime(LocalTime.parse(forecastItem.getBaseTime(), timeFormatter))
-                                    .category(forecastItem.getCategory())
-                                    .fcstDate(LocalDate.parse(forecastItem.getFcstDate(), dateFormatter))
-                                    .fcstTime(LocalTime.parse(forecastItem.getFcstTime(), timeFormatter))
-                                    .fcstValue(Integer.parseInt(forecastItem.getFcstValue()))
-                                    .nx(forecastItem.getNx())
-                                    .ny(forecastItem.getNy())
-                                    .build();
-                        }).toList();
-                shortForecastRepository.saveAll(forecastItems);
+            if (response == null) {
+                throw new ShortForecastException("단기예보 API 요청에 대한 응답을 받지 못했습니다");
             }
+
+            if (!"00".equals(response.getResponse().getHeader().getResultCode())) {   //00은 정상 응답 시 받는 code.
+
+                throw new ShortForecastException(response.getResponse().getHeader().getResultMsg());
+            }
+
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+            DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HHmm");
+
+            List<ShortForeCastResponseDTO.Response.Body.Items.ForecastItem> itemList = response.getResponse().getBody().getItems().getItem();
+
+            List<ShortForecast> forecastItems = itemList.stream()
+                    .map(forecastItem -> {
+                        return ShortForecast.builder()
+                                .baseDate(LocalDate.parse(forecastItem.getBaseDate(), dateFormatter))
+                                .baseTime(LocalTime.parse(forecastItem.getBaseTime(), timeFormatter))
+                                .category(forecastItem.getCategory())
+                                .fcstDate(LocalDate.parse(forecastItem.getFcstDate(), dateFormatter))
+                                .fcstTime(LocalTime.parse(forecastItem.getFcstTime(), timeFormatter))
+                                .fcstValue(forecastItem.getFcstValue())
+                                .nx(forecastItem.getNx())
+                                .ny(forecastItem.getNy())
+                                .build();
+                    }).toList();
+            shortForecastRepository.saveAll(forecastItems);
 
         } catch (WebClientResponseException ex) {
             if (ex.getStatusCode().is4xxClientError()) {
-                throw new CustomClientException("단기예보 api 호출 중 client 에러 발생", ex);
+                log.error(ex.getMessage());
+                throw new ShortForecastException("단기예보 api 호출 중 client 에러 발생", ex);
+
             } else if (ex.getStatusCode().is5xxServerError()) {
-                throw new CustomServerException("단기예보 api 호출 중 server 에러 발생", ex);
+                log.error(ex.getMessage());
+                throw new ShortForecastException("단기예보 api 호출 중 server 에러 발생", ex);
+
             } else {
+                log.error(ex.getMessage());
                 throw new RuntimeException("예상치 못한 에러 발생", ex);
             }
         }

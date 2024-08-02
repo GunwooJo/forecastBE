71f7f35a8e96302864d7c3e19ee5f760c1185cd9
GunwooJo <kanggi1997@naver.com>
2024-08-02 21:36:56 +0900

    Feat: Region의 좌표 가져오는 메서드

--- src/main/java/site/gunwoo/forecastBE/repository/RegionRepository.java
+++ src/main/java/site/gunwoo/forecastBE/repository/RegionRepository.java
@@ -1,9 +1,15 @@
 package site.gunwoo.forecastBE.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import site.gunwoo.forecastBE.entity.Region;
+
+import java.util.List;
 
 public interface RegionRepository extends JpaRepository<Region, Long> {
 
     Region findByName(String name);
+
+    @Query("SELECT DISTINCT r.xPos, r.yPos FROM Region r")
+    List<Object[]> findDistinctByXPosAndYPos();
 }

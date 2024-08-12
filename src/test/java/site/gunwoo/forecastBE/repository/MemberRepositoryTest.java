5404555f95474a44995fe92591af5a15f936414c
GunwooJo <kanggi1997@naver.com>
2024-08-12 16:42:11 +0900

    Test: region 테이블에 존재하지 않는 지역 이름으로 가입을 시도하면 예외를 발생시킨다.

--- src/test/java/site/gunwoo/forecastBE/repository/MemberRepositoryTest.java
+++ src/test/java/site/gunwoo/forecastBE/repository/MemberRepositoryTest.java
@@ -6,10 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import site.gunwoo.forecastBE.dto.MemberJoinDTO;
-import site.gunwoo.forecastBE.entity.Region;
 import site.gunwoo.forecastBE.service.MemberService;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -24,7 +24,7 @@ class MemberRepositoryTest {
     @Test
     void join() {
         //given
-        List<String> regions = List.of("테스트32313");
+        List<String> regions = List.of("테스트");
         MemberJoinDTO member1 = MemberJoinDTO.builder()
                 .email("test1@naver.com")
                 .password("gunwoo1234!")
@@ -42,5 +42,22 @@ class MemberRepositoryTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이미 존재하는 이메일입니다: " + member2.getEmail());
 
+    }
+
+    @DisplayName("region 테이블에 존재하지 않는 지역 이름으로 가입을 시도하면 예외를 발생시킨다.")
+    @Test
+    void joinWithNonExistentRegion() {
+        //given
+        List<String> regions = List.of("test", "존재하지않는지역명");
+        MemberJoinDTO member = MemberJoinDTO.builder()
+                .email("test1@naver.com")
+                .password("gunwoo1234!")
+                .regions(regions)
+                .build();
+
+        //when, then
+        assertThatThrownBy(() -> memberService.join(member))
+                .isInstanceOf(NoSuchElementException.class)
+                .hasMessage("존재하지 않는 지역입니다.");
     }
 }

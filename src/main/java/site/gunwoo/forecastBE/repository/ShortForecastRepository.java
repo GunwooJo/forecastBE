2c60644e997f1124cfed2c49af5ab8d5f96ac1df
GunwooJo <kanggi1997@naver.com>
2024-08-21 22:51:14 +0900

    Feat: baseDate, baseTime, nx, ny로 단기예보 데이터 조회

--- src/main/java/site/gunwoo/forecastBE/repository/ShortForecastRepository.java
+++ src/main/java/site/gunwoo/forecastBE/repository/ShortForecastRepository.java
@@ -1,15 +1,22 @@
 package site.gunwoo.forecastBE.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import site.gunwoo.forecastBE.entity.ShortForecast;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
-
+@Repository
 public interface ShortForecastRepository extends JpaRepository<ShortForecast, Long> {
 
     @Transactional
     void deleteShortForecastByCreatedAtBefore(LocalDateTime dateTime);
+
+    @Query("select s from ShortForecast s where s.baseDate = :baseDate and s.baseTime = :baseTime and s.nx = :nx and s.ny = :ny")
+    List<ShortForecast> findShortForecast(LocalDate baseDate, LocalTime baseTime, int nx, int ny);
 }

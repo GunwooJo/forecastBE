b459e831a141cf5b1737743700b4bf2a91ee9f0f
GunwooJo <kanggi1997@naver.com>
2024-08-28 19:00:01 +0900

    Feat: 단기예보 API호출 시 사용할 Custom Exception 작성

---
+++ src/main/java/site/gunwoo/forecastBE/exception/ShortForecastApiException.java
@@ -0,0 +1,28 @@
+package site.gunwoo.forecastBE.exception;
+
+import lombok.Getter;
+
+/*
+단기예보 API를 호출할 때 오류가 발생한 경우 다음 예외를 사용하세요.
+ */
+@Getter
+public class ShortForecastApiException extends RuntimeException{
+
+    private String errorCode;   //API 응답으로 받은 resultCode
+    private String errorMsg;    //API 응답으로 받은 resultMsg
+
+    public ShortForecastApiException() {
+    }
+
+    public ShortForecastApiException(String message, String errorCode, String errorMsg) {
+        super(message);
+        this.errorCode = errorCode;
+        this.errorMsg = errorMsg;
+    }
+
+    public ShortForecastApiException(String message, String errorCode, String errorMsg, Throwable cause) {
+        super(message, cause);
+        this.errorCode = errorCode;
+        this.errorMsg = errorMsg;
+    }
+}

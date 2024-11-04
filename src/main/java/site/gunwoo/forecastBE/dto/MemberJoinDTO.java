7ed171edca13278fba0deca02b0ee587949fb639
GunwooJo <kanggi1997@naver.com>
2024-11-04 17:11:57 +0900

    Refactor: 회원가입 요청 파라미터 검증 수정

--- src/main/java/site/gunwoo/forecastBE/dto/MemberJoinDTO.java
+++ src/main/java/site/gunwoo/forecastBE/dto/MemberJoinDTO.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
+import site.gunwoo.forecastBE.dto.region.RegionNameDTO;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,16 +14,20 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.NONE)
 public class MemberJoinDTO {
 
-    @NotBlank
+    @NotBlank(message = "이메일은 필수 값입니다.")
     @Email
     private String email;
 
-    @NotEmpty
+    @NotEmpty(message = "비밀번호는 필수 값입니다.")
     private String password;
 
+    @NotEmpty(message = "지역을 선택해야 합니다.")
+    private List<RegionNameDTO> regions = new ArrayList<>();
+
     @Builder
-    public MemberJoinDTO(String email, String password, List<String> regions) {
+    public MemberJoinDTO(String email, String password, List<RegionNameDTO> regions) {
         this.email = email;
         this.password = password;
+        this.regions = regions;
     }
 }

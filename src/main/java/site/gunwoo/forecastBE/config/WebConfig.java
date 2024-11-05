6f329316d948060e8a28b0f382cdd2251283d639
GunwooJo <kanggi1997@naver.com>
2024-11-05 16:54:44 +0900

    Refactor: cors 설정 수정.

--- src/main/java/site/gunwoo/forecastBE/config/WebConfig.java
+++ src/main/java/site/gunwoo/forecastBE/config/WebConfig.java
@@ -16,9 +16,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("Authorization", "Content-Type")
-                .exposedHeaders("Custom-Header")
                 .allowCredentials(true)
                 .maxAge(3600);
     }

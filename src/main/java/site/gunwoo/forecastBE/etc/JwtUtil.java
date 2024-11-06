da5e36c5d11dab04d53404707cc0178172639228
GunwooJo <kanggi1997@naver.com>
2024-11-06 10:20:14 +0900

    Feat: jwt 관련 메서드 작성

---
+++ src/main/java/site/gunwoo/forecastBE/etc/JwtUtil.java
@@ -0,0 +1,84 @@
+package site.gunwoo.forecastBE.etc;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import site.gunwoo.forecastBE.dto.MemberDTO;
+
+import java.security.Key;
+import java.util.Date;
+
+@Slf4j
+@Component
+public class JwtUtil {
+
+    private final Key key;
+    private final long accessTokenExpTime;
+
+    public JwtUtil(
+            @Value("${jwt.secret}") String secretKey,
+            @Value("${jwt.expiration_time}") long accessTokenExpTime
+    ) {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+        this.accessTokenExpTime = accessTokenExpTime;
+    }
+
+    private String createAccessToken(MemberDTO member) {
+        Claims claims = Jwts.claims();
+        claims.put("email", member.getEmail());
+
+        return Jwts.builder()
+                .setIssuer("forecastBE")
+                .setClaims(claims)
+                .setSubject(member.getEmail())
+                .setExpiration(new Date(System.currentTimeMillis() + accessTokenExpTime))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+    // token에서 email 추출
+    public String getUserEmail(String token) {
+        return parseClaims(token).get("email", String.class);
+    }
+
+    /**
+     * JWT 검증
+     * @param token
+     * @return IsValidate
+     */
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.info("Invalid JWT Token", e);
+        } catch (ExpiredJwtException e) {
+            log.info("Expired JWT Token", e);
+        } catch (UnsupportedJwtException e) {
+            log.info("Unsupported JWT Token", e);
+        } catch (IllegalArgumentException e) {
+            log.info("JWT claims string is empty.", e);
+        }
+        return false;
+    }
+
+    /**
+     * JWT Claims 추출
+     * @param accessToken
+     * @return JWT Claims
+     */
+    public Claims parseClaims(String accessToken) {
+        try {
+            return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(accessToken).getBody();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
+    }
+
+
+}

9576b8af016c1b2c39b1d10e5e820e052f11737a
GunwooJo <kanggi1997@naver.com>
2024-11-08 08:58:03 +0900

    Refactor: 현재 시각이 오전 12시 ~ 12시 46분일 경우 api 호출할 때 파라미터 날짜 수정

--- src/main/java/site/gunwoo/forecastBE/etc/ShortForecastScheduler.java
+++ src/main/java/site/gunwoo/forecastBE/etc/ShortForecastScheduler.java
@@ -16,6 +16,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
+//TODO: 00시 ~ 00시 45분 사이에는 하루 전의 baseDate를 사용하고 baseTime도 수정해야함
 @Component
 @RequiredArgsConstructor
 public class ShortForecastScheduler {
@@ -47,6 +48,12 @@ public class ShortForecastScheduler {
             DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HHmm");
             LocalTime now = LocalTime.now();
             LocalTime closestPast46Minute = now.withMinute(46).isBefore(now) ? now.withMinute(46) : now.withMinute(46).minusHours(1);
+
+            // 오전 12시 ~ 12시 45분 사이의 경우, 날짜를 하루 전으로 설정
+            if (now.isBefore(LocalTime.of(0, 46))) {
+                currentDate = LocalDate.now().minusDays(1).format(dateFormatter);
+            }
+
             String currentTime = closestPast46Minute.format(timeFormatter);
 
             shortForecastService.saveShortForecast(currentDate, currentTime, 60, 1, xPos, yPos);

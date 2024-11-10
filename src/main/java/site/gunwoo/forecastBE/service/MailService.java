36a5c0f539a8d9085fe44605500cff0c954191ed
GunwooJo <kanggi1997@naver.com>
2024-11-10 20:59:35 +0900

    Refactor: 메일 로그 추가

--- src/main/java/site/gunwoo/forecastBE/service/MailService.java
+++ src/main/java/site/gunwoo/forecastBE/service/MailService.java
@@ -1,6 +1,7 @@
 package site.gunwoo.forecastBE.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -8,11 +9,15 @@ import site.gunwoo.forecastBE.dto.MailDTO;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MailService {
 
     private final JavaMailSender javaMailSender;
 
     public void sendMail(MailDTO mailDTO){
+        log.info("받는 사람: {}", mailDTO.getAddress());
+        log.info("메일 제목: {}", mailDTO.getTitle());
+        log.info("메일 내용: {}", mailDTO.getMessage());
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(mailDTO.getAddress());
         message.setSubject(mailDTO.getTitle());

d87d3e0051136089be7fd5d007a5a74a5b8d270c
GunwooJo <kanggi1997@naver.com>
2024-11-11 18:10:48 +0900

    Refactor: 컨트롤러 응답 수정

--- src/main/java/site/gunwoo/forecastBE/controller/AlertController.java
+++ src/main/java/site/gunwoo/forecastBE/controller/AlertController.java
@@ -2,12 +2,15 @@ package site.gunwoo.forecastBE.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import site.gunwoo.forecastBE.dto.AlertSettingDTO;
+import site.gunwoo.forecastBE.dto.ResponseDTO;
 import site.gunwoo.forecastBE.service.AlertService;
 
 @RestController
@@ -17,10 +20,11 @@ public class AlertController {
     private final AlertService alertService;
 
     @PostMapping("/alert/setting")
-    public void setAlert(@RequestBody @Valid AlertSettingDTO alertSettingDTO) {
+    public ResponseEntity<ResponseDTO> setAlert(@RequestBody @Valid AlertSettingDTO alertSettingDTO) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String userEmail = authentication.getName();
 
         alertService.setAlert(userEmail, alertSettingDTO);
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseDTO("날씨 알림 설정 완료", null));
     }
 }

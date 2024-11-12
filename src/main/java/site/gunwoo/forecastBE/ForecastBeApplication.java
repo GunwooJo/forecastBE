7c867d3857b37d90ac0cb962a69dd06f6564952d
GunwooJo <kanggi1997@naver.com>
2024-11-13 00:53:29 +0900

    Feat: 타임존을 서울로 설정

--- src/main/java/site/gunwoo/forecastBE/ForecastBeApplication.java
+++ src/main/java/site/gunwoo/forecastBE/ForecastBeApplication.java
@@ -1,9 +1,13 @@
 package site.gunwoo.forecastBE;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.time.LocalDateTime;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -14,4 +18,9 @@ public class ForecastBeApplication {
 		SpringApplication.run(ForecastBeApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		System.out.println(">>> 현재 시간은 " + LocalDateTime.now());
+	}
 }

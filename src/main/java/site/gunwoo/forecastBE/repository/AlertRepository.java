2c2eab7f62b72312ef49c061c8a991de8c67adff
GunwooJo <kanggi1997@naver.com>
2024-11-13 12:33:03 +0900

    Refactor: fetch join으로 최적화

--- src/main/java/site/gunwoo/forecastBE/repository/AlertRepository.java
+++ src/main/java/site/gunwoo/forecastBE/repository/AlertRepository.java
@@ -4,10 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import site.gunwoo.forecastBE.entity.Alert;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AlertRepository extends JpaRepository<Alert, Long> {
 
     @Query("select a from Alert a join fetch a.member m where a.id = :id")
     Optional<Alert> findByIdWithMember(Long id);
+
+    @Query("select a from Alert a join fetch a.member m")
+    List<Alert> findAllWithMember();
 }
